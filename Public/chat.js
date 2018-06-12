2e1a8bf821cb31fc0ca3ebe15e1d69df61fc4a83
Vasudev prajapat <parth21995@gmail.com>
2018-06-12 22:40:32 +0530

    Add files via upload

---
+++ Public/chat.js
@@ -0,0 +1,30 @@
+//make connection//make connection
+var socket=io.connect('http://localhost:4000');
+//dom query
+var message=document.getElementById('message');
+var handle=document.getElementById('handle');
+ var btn=document.getElementById('send');
+var output=document.getElementById('output');
+var feedback=document.getElementById('feedback');
+//emit event
+
+btn.addEventListener('click',function(){
+	socket.emit('chat',{
+	 message: message.value,
+		handle: handle.value
+	});
+	// body...
+});
+message.addEventListener('keypress', function() {
+socket.emit('typing', handle.value)	;
+	// body...
+});
+
+//listen for events
+socket.on('chat',function (data) {
+	output.innerHTML +='<p><strong>'+ data.handle +':</strong>' + data.message +'</p>';
+	// body...
+});
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
