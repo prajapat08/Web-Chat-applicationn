d313bb38ef07405a6f2427ac486fd14a2fe210ea
Vasudev prajapat <parth21995@gmail.com>
2018-06-30 17:22:31 +0530

    Update chat.js

--- Public/chat.js
+++ Public/chat.js
@@ -23,7 +23,7 @@ socket.emit('typing', handle.value)	;
 //listen for events
 socket.on('chat',function (data) {
 	output.innerHTML +='<p><strong>'+ data.handle +':</strong>' + data.message +'</p>';
-	// body...
+	 
 });
 socket.on('typing', function(data){
     feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
